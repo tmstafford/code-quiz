b868b5095c9456bd2e34b9ab0f8f96daa24b909a
tmstafford <tatumstafford1@gmail.com>
2020-10-16 19:05:19 -0500

    questions array and js psuedocode

---
+++ assets/script.js
@@ -0,0 +1,53 @@
+// var quiz questions as array 
+var quizQuestions = [
+    {
+        title: "Commonly used data types do NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed with _______.",
+        choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        answer: "parenthesis"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ________.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within _______ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "parenthesis"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    },
+];
+
+// var for: start button 
+// var for: questions, current question, index, timer, time left, choice buttons, 
+// correct answer, wrong answer
+
+// function to start the quiz (hide display of start page)
+// start timer countdown (setInterval)
+
+// function with for to loop through question arrays
+
+// function for correct and incorrect statements (decreasing time too)
+
+// function to end quiz (if out of time (or it ends but probably in for loop))
+
+// function to get the score after quiz ends
+// form to enter initials, store with score
+// high score? local storage? 
+
+// go back or view high score buttons after initials 
+
+// refresh page reloading quiz again 
+
+
+// need to figure out viewing high score when header link is clicked
+// also 
